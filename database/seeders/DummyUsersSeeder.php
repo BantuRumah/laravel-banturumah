1455cd1cd9ceeed7578b7e56975d5bf61ab1d01e
arvandha121 <nauvan121@gmail.com>
2023-10-22 20:57:14 +0700

    foreign key table roles ke table users

--- database/seeders/DummyUsersSeeder.php
+++ database/seeders/DummyUsersSeeder.php
@@ -15,54 +15,66 @@ class DummyUsersSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $adminRole = Role::where('role', 'admin')->first();
-        // $mitraRole = Role::where('role', 'mitra')->first();
-        // $userRole = Role::where('role', 'user')->first();
-        // User::create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => Hash::make('password'),
-        //     'role' => $adminRole->id,
-        // ]);
 
-        // User::create([
-        //     'name' => 'Mitra',
-        //     'email' => 'mitra@gmail.com',
-        //     'password' => Hash::make('password'),
-        //     'role' => $mitraRole->id,
-        // ]);
+     public function run()
+     {
+         $userData = [
+             [
+                 'name' => 'Admin BantuRumah',
+                 'email' => 'banturumah4@gmail.com',
+                 'password' => Hash::make('Password121'),
+                 'role' => 'admin',
+             ],
+             [
+                 'name' => 'Mitra BantuRumah',
+                 'email' => 'mitra@gmail.com',
+                 'password' => Hash::make('Password121'),
+                 'role' => 'mitra',
+             ],
+             [
+                 'name' => 'User BantuRumah',
+                 'email' => 'user@gmail.com',
+                 'password' => Hash::make('Password121'),
+                 'role' => 'user',
+             ],
+         ];
+     
+         foreach ($userData as $data) {
+             $user = User::create([
+                 'name' => $data['name'],
+                 'email' => $data['email'],
+                 'password' => $data['password'],
+                 'role' => $data['role'],
+                //  'role' => Role::where('role', $data['role'])->first()->id,
+             ]);
+         }
+     }
+     
+    // public function run()
+    // {
+    //     $userData = [
+    //         [
+    //             'name' => 'Admin BantuRumah',
+    //             'email' => 'banturumah4@gmail.com',
+    //             'password' => Hash::make('Password121'),
+    //             'role' => 'admin',
+    //         ],
+    //         [
+    //             'name' => 'Mitra BantuRumah',
+    //             'email' => 'mitra@gmail.com',
+    //             'password' => Hash::make('Password121'),
+    //             'role' => 'mitra',
+    //         ],
+    //         [
+    //             'name' => 'User BantuRumah',
+    //             'email' => 'user@gmail.com',
+    //             'password' => Hash::make('Password121'),
+    //             'role' => 'user',
+    //         ],
+    //     ];
 
-        // User::create([
-        //     'name' => 'User',
-        //     'email' => 'user@gmail.com',
-        //     'password' => Hash::make('password'),
-        //     'role' => $userRole->id,
-        // ]);
-        $userData = [
-            [
-                'name' => 'Admin BantuRumah',
-                'email' => 'banturumah4@gmail.com',
-                'password' => Hash::make('Password121'),
-                'role' => 'admin',
-            ],
-            [
-                'name' => 'Mitra BantuRumah',
-                'email' => 'mitra@gmail.com',
-                'password' => Hash::make('Password121'),
-                'role' => 'mitra',
-            ],
-            [
-                'name' => 'User BantuRumah',
-                'email' => 'user@gmail.com',
-                'password' => Hash::make('Password121'),
-                'role' => 'user',
-            ],
-        ];
-
-        foreach($userData as $key => $value) {
-            User::create($value);
-        }
-    }
+    //     foreach($userData as $key => $value) {
+    //         User::create($value);
+    //     }
+    // }
 }
